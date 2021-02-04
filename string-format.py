555c3f9c73e2eafc4afb2f6231be41c183b6be51
emirhan sirinoglu <srnoglu98@gmail.com>
2021-02-04 21:21:12 +0300

    stringformatı

---
+++ string-format.py
@@ -0,0 +1,16 @@
+isim = "Emirhan"
+soyisim = "ŞİRİNOĞLU"
+yası = 22
+memleketi = "Kocaeli"
+
+tanitim = ("Benim adım " + isim + " " + soyisim + " yaşım " + str(yası)+ " memleketim " + memleketi)
+print(tanitim)
+
+---
+
+
+isim = "Emirhan"
+soyisim = "Sirinoglu"
+print("Benim ismin {}" .format(isim,soyisim))
+
+print("benim ismim {i} {s}" .format(i = isim,s = soyisim))
