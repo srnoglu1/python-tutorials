b804a79b77da499aea5d913da144d8768ad728aa
emirhan sirinoglu <srnoglu98@gmail.com>
2021-02-06 15:15:51 +0300

    my-python-trials

---
+++ string-format-two.py
@@ -0,0 +1,13 @@
+ university = "Kocaeli Üniversitesi"
+ sonuc = university[0:7]
+ print (sonuc)
+
+ sonuc1 = university[-12: ]
+ print (sonuc1)
+
+ sonuc2 = university[::2]
+ print (sonuc2) 
+
+ a = "Türkiye"
+ b = 6*(a)
+ print(b)
